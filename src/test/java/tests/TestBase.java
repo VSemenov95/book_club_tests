285678bcba5f547e591ec77a05c024ceb76228f7
homeWork_1.12 <semasvs18@gmail.com>
2026-08-22 14:44:43 +0300

    add configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,13 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import api.ApiClient;
 import models.registration.SuccessfulRegistrationResponseModel;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static data.TestData.REGISTRATION_IP_REGEXP;
 import static io.qameta.allure.Allure.step;
@@ -17,6 +21,13 @@ public class TestBase {
     public static void setUp() {
         RestAssured.baseURI = "https://book-club.qa.guru";
         RestAssured.basePath = "/api/v1";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127");
+        Configuration.remote = System.getProperty("https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of("enableVNC", true, "enableVideo", true));
+        Configuration.browserCapabilities = capabilities;
     }
 
     public static void checkSuccessfulRegistrationResponse(SuccessfulRegistrationResponseModel response,

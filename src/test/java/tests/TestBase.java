667dac2a1494e2a4070f420d2c053767c201f576
homeWork_1.12 <semasvs18@gmail.com>
2026-08-22 22:07:00 +0300

    change configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -8,6 +8,9 @@ import io.restassured.RestAssured;
 import models.registration.SuccessfulRegistrationResponseModel;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -24,14 +27,57 @@ public class TestBase {
     public static void setUp() {
         RestAssured.baseURI = "https://book-club.qa.guru";
         RestAssured.basePath = "/api/v1";
-        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.remote = System.getProperty("https://qa_engineer:-aAb_-4gs53FD@selenoid.qa.guru/wd/hub");
+        Configuration.browser = System.getProperty("browser", "chrome").toLowerCase();
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "149");
-        Configuration.remote = System.getProperty("https://qa_engineer:-aAb_-4gs53FD@selenoid.qa.guru/wd/hub");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of("enableVNC", true, "enableVideo", true));
-        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://book-club.qa.guru");
+        Configuration.timeout = 15000;
+        Configuration.pageLoadTimeout = 60000;
+        Configuration.pageLoadStrategy = "eager";
+
+    String browserVersion = System.getProperty("browserVersion");
+        if (browserVersion != null && !browserVersion.isBlank()) {
+        Configuration.browserVersion = browserVersion;
     }
+
+    String remoteUrl = System.getProperty("remoteUrl");
+        if (remoteUrl == null || remoteUrl.isBlank()) {
+        String login = System.getProperty("remoteBrowserUrlLogin");
+        String password = System.getProperty("remoteBrowserUrlPassword");
+        if (login != null && password != null && !login.isBlank() && !password.isBlank()) {
+            remoteUrl = "https://" + login + ":" + password + "@" +
+                    System.getProperty("remoteBrowserUrl", "selenoid.autotests.cloud/wd/hub");
+        }
+    }
+
+    boolean remote = remoteUrl != null && !remoteUrl.isBlank();
+    Configuration.headless = !remote && Boolean.parseBoolean(System.getProperty("headless", "false"));
+
+        if (remote) {
+        Configuration.remote = remoteUrl;
+        Configuration.browserCapabilities = selenoidCapabilities();
+    }
+}
+
+private static MutableCapabilities selenoidCapabilities() {
+    Map<String, Object> selenoidOptions = Map.of(
+            "enableVNC", true,
+            "enableVideo", true
+    );
+    if ("chrome".equalsIgnoreCase(Configuration.browser)) {
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+        options.setCapability("selenoid:options", selenoidOptions);
+        return options;
+    }
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("pageLoadStrategy", "eager");
+    capabilities.setCapability("selenoid:options", selenoidOptions);
+    return capabilities;
+}
 
     @BeforeEach
     public void setup() {

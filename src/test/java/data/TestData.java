38c653cae58f19e78165c11b62b1c78e26932fe9
homeWork_1.12 <semasvs18@gmail.com>
2026-08-23 00:44:00 +0300

    fix TestData

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 
 public class TestData {
     Faker faker = new Faker(new Locale("en"));
-    public String username = faker.name().firstName() + "vssemenov_tests" + faker.planet(),
+    public String username = faker.name().firstName() + "vssemenov_tests",
             password = faker.credentials().password(),
             longUsername = faker.lorem().characters(151),
             usernameIsMaxCharacters = faker.lorem().characters(150),
